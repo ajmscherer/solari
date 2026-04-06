4362712a384bfae0e1385ed667b827b54492a85b
Alex Scherer <alexandre.scherer@outlook.com>
2026-04-06 14:40:12 -0400

    simplify and prune code, FullScreen capability

--- code/grkivy.py
+++ code/grkivy.py
@@ -23,39 +23,30 @@ class KiviGraphicInterface(grabst.GraphicInterface):
             timeStamp = datetime.datetime.now()
 
             # retrieve the Kivy canvas
-            kivyCanvas = self.kivi_app.root.canvas if self.kivi_app.root else None
+            root = self.kivi_app.root
+            if root:
+                kivyCanvas = root.canvas 
 
-            if kivyCanvas:
+                if kivyCanvas:
+                    # clear the Kivy object canvas
+                    kivyCanvas.clear()
 
-                # clear the Kivy object canvas
-                kivyCanvas.clear()
-                
-                # retrieve actual size of the Widget
-                actualsizeSize = self.kivi_app.root.size if self.kivi_app.root else None
-                if actualsizeSize:
-                    width, height = actualsizeSize
-                else:
-                    width, height = sizeRequirement
+                    scaleFactor = 1.0
 
+                    with kivyCanvas:
 
-                # calculate scale factor based on required space
-                widthRequired, heightRequired = sizeRequirement
-                scaleFactor = min(width/widthRequired, height/heightRequired)
-
-                with kivyCanvas:
-
-                    kivy.graphics.PushMatrix()
-                    self.kivi_app.scale=kivy.graphics.Scale(scaleFactor)
+                        kivy.graphics.PushMatrix()
+                        self.kivi_app.scale=kivy.graphics.Scale(scaleFactor)
 
 
-                # build a canvas on the fly based on kivy
-                canvas = CanvasWrapperKivy(kivyCanvas=kivyCanvas, width=width/scaleFactor, height=height/scaleFactor)
-                
-                # call the drawing function of the graphic app
-                drawFunction(canvas, timeStamp)
+                    # build a canvas on the fly based on kivy
+                    canvas = CanvasWrapperKivy(kivyCanvas=kivyCanvas, width=root.width, height=root.height)
+                    
+                    # call the drawing function of the graphic app
+                    drawFunction(canvas, timeStamp)
 
-                with kivyCanvas:
-                    kivy.graphics.PopMatrix()
+                    with kivyCanvas:
+                        kivy.graphics.PopMatrix()
 
         kivy.clock.Clock.schedule_interval(update, 1.0 / framePerSecond)
         
@@ -64,8 +55,18 @@ class KiviGraphicInterface(grabst.GraphicInterface):
         if sizeRequirement:
             window.size = sizeRequirement 
 
+        # bind keyboard events
+        kivy.core.window.Window.bind(on_key_down=self.on_keyboard)
+
         self.kivi_app.run()
-        
+    
+    def on_keyboard(self, window, key, scancode, codepoint, modifier):
+        # press 'f' to toggle fullscreen
+        if codepoint == 'f':
+            window.fullscreen = not window.fullscreen
+            return True
+        return False
+
     def setTitle(self, title):
         self.kivi_app.title = title
 
@@ -128,105 +129,3 @@ class CanvasWrapperKivy(grabst.Canvas):
             r, g, b = [ int(v.hex(),16) / 255.0 for v in color.getRGB()]
             kivy.graphics.Color(r, g, b, opacity)  # Set the color to (RGBA)
             
-
-class KMainWidget(kivy.uix.widget.Widget):
-
-    def __init__(self, app, framePerSecond,  **kwargs):
-        super(KMainWidget, self).__init__(**kwargs)
-
-        # store drawingMethod
-        self.app = app
-        self.rotation = 0
-
-        # schedule redraw at regular interval
-        kivy.clock.Clock.schedule_interval(self.update, 1.0 / framePerSecond)
-
-
-    def update(self, timeInterval):
-
-        # get a timeStamp for the drawing request
-        timeStamp = datetime.datetime.now()
-
-        # retrieve the Kivy canvas
-        kivyCanvas = self.canvas
-
-        if kivyCanvas:
-
-            # clear the Kivy object canvas
-            kivyCanvas.clear()
-            
-            # retrieve size of the Widget
-            width, height = self.size
-
-
-            # calculate scale factor based on required space
-            widthRequired, heightRequired = self.app.sizeRequirement
-            scaleFactor = min(width/widthRequired, height/heightRequired)
-
-            with kivyCanvas:
-
-                '''
-                s = (datetime.datetime.now().second * 4) % 100
-                kivy.graphics.Line(points=(s,s,width-s,height-s))
-                kivy.graphics.Line(points=(s,height-s,width-s,s))
-                '''
-
-                kivy.graphics.PushMatrix()
-                self.scale=kivy.graphics.Scale(scaleFactor)
-
-
-            # build a canvas on the fly based on kivy
-            canvas = CanvasWrapperKivy(kivyCanvas=kivyCanvas, width=width/scaleFactor, height=height/scaleFactor)
-            
-            # call the drawing function of the graphic app
-            self.app.drawMainWindow(canvas, timeStamp)
-
-            with kivyCanvas:
-                kivy.graphics.PopMatrix()
-
-
-
-class KApp(kivy.app.App):
-
-    def __init__(self, graphicApp, **kwargs):
-        super().__init__(**kwargs)
-        self.graphicApp = graphicApp
-
-    def build(self):
-        app = self.graphicApp
-        widget =  KMainWidget(app, app.framePerSecond)
-        window = kivy.core.window.Window
-        srs = 'sizeRequirement'
-        if srs in app.__dict__:
-            width, height = app.__dict__[srs]
-            window.size = width, height
-        else:
-            window.fullscreen = True
-        kivy.core.window.Window.bind(on_resize=self.on_window_resize)
-        return widget
-
-    def on_window_resize(self, window, width, height):
-        # You can add logic here to handle window resize events if needed
-        pass
-
-"""
-class KivyApp(grabst.App):
-
-    def __init__(self, framePerSecond, sizeRequirement, title) -> None:
-        super().__init__(framePerSecond, sizeRequirement)
-        self.title = title
-        self.kapp = KApp(self)
-        self.kapp.title = self.title
-   
-    def _run(self):
-        '''Implementation of abstract _run method'''
-        self.kapp.run()
-
-
-class KivyFactory(grabst.Factory):
-    APP = KivyApp
-
-
-
-"""
-    
