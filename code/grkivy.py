e858091fa7d6ec751daa6f8c75c1079765314eb2
Alex Scherer <alexandre.scherer@outlook.com>
2026-04-23 08:29:22 -0400

    args

--- code/grkivy.py
+++ code/grkivy.py
@@ -15,6 +15,9 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # 
 
+import os
+os.environ.setdefault('KIVY_NO_ARGS', '1')
+
 import grabst
 import kivy
 import kivy.app
